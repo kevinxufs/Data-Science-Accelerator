514fcec9961025a20bc66cd3f6196daf80894fc7
kevinxufs <48526846+kevinxufs@users.noreply.github.com>
2019-03-15 16:12:54 +0000

    Create app.R

---
+++ ml_app_files/app.R
@@ -0,0 +1,116 @@
+library(caret)
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+
+# setwd('~/Desktop/ml_app')
+
+draw_confusion_matrix <- function(cm) {
+  
+  layout(matrix(c(1,1,2)))
+  par(mar=c(2,2,2,2))
+  plot(c(100, 345), c(300, 450), type = "n", xlab="", ylab="", xaxt='n', yaxt='n')
+  title('CONFUSION MATRIX', cex.main=2)
+  
+  # create the matrix 
+  rect(150, 430, 240, 370, col='#3F97D0')
+  text(195, 435, 'Exceed', cex=1.2)
+  rect(250, 430, 340, 370, col='#F7AD50')
+  text(295, 435, 'Not Exceed', cex=1.2)
+  text(125, 370, 'Predicted', cex=1.3, srt=90, font=2)
+  text(245, 450, 'Actual', cex=1.3, font=2)
+  rect(150, 305, 240, 365, col='#F7AD50')
+  rect(250, 305, 340, 365, col='#3F97D0')
+  text(140, 400, 'Exceed', cex=1.2, srt=90)
+  text(140, 335, 'Not Exceed', cex=1.2, srt=90)
+  
+  # add in the cm results 
+  res <- as.numeric(cm$table)
+  text(195, 400, res[1], cex=1.6, font=2, col='white')
+  text(195, 335, res[2], cex=1.6, font=2, col='white')
+  text(295, 400, res[3], cex=1.6, font=2, col='white')
+  text(295, 335, res[4], cex=1.6, font=2, col='white')
+  
+  # add in the specifics 
+  plot(c(100, 0), c(100, 0), type = "n", xlab="", ylab="", main = "DETAILS", xaxt='n', yaxt='n')
+  text(10, 85, names(cm$byClass[1]), cex=1.2, font=2)
+  text(10, 70, round(as.numeric(cm$byClass[1]), 3), cex=1.2)
+  text(30, 85, names(cm$byClass[2]), cex=1.2, font=2)
+  text(30, 70, round(as.numeric(cm$byClass[2]), 3), cex=1.2)
+  text(50, 85, names(cm$byClass[5]), cex=1.2, font=2)
+  text(50, 70, round(as.numeric(cm$byClass[5]), 3), cex=1.2)
+  text(70, 85, names(cm$byClass[6]), cex=1.2, font=2)
+  text(70, 70, round(as.numeric(cm$byClass[6]), 3), cex=1.2)
+  text(90, 85, names(cm$byClass[7]), cex=1.2, font=2)
+  text(90, 70, round(as.numeric(cm$byClass[7]), 3), cex=1.2)
+  
+  # add in the accuracy information 
+  text(30, 35, names(cm$overall[1]), cex=1.5, font=2)
+  text(30, 20, round(as.numeric(cm$overall[1]), 3), cex=1.4)
+  text(70, 35, names(cm$overall[2]), cex=1.5, font=2)
+  text(70, 20, round(as.numeric(cm$overall[2]), 3), cex=1.4)
+}  
+
+
+varimp_set <- readRDS('varimp_set.rds')
+
+cm_set <- readRDS('cm_set.rds')
+
+
+
+ui <- fluidPage(
+  selectInput('dataset', 'Choose Dataset',  choices = c('Random forest' = '1', 
+                                                        
+                                                        'Neural network'= '2',
+                                                        'Multivariate adaptive regression splines' = '3',
+                                                        
+                                                        'Unbiased multivariate adaptive regression splines' = '4',
+                                                        'Flexible Discriminant Analysis' = '5',
+                                                        'K nearest neighbour' = '6',
+                                                        'Logit boost' = '7'
+                                                        
+  )),
+  plotOutput('graph'),
+  plotOutput('varImp')
+)
+
+server = function(input, output, session){
+  
+  # outVar <- reactive({
+  #   temp <- datafiles[[as.numeric(input$dataset)]]
+  #   temp <- confusionMatrix(as.factor(temp$pred), as.factor(testSet$is_exceed))
+  #   
+  #  
+  # 
+  # })
+  # 
+  
+  output$graph <- renderPlot({
+    draw_confusion_matrix(cm_set[[as.numeric(input$dataset)]])
+  })
+  
+  outImp <- reactive({
+    temp <- modelfiles[[as.numeric(input$dataset)]]
+    
+  })
+  
+  
+  
+  output$varImp <- renderPlot({
+    
+    req(input$dataset %in% c(1:5)) 
+    
+    plot(varimp_set[[as.numeric(input$dataset)]], top = 10, main = 'VARIABLE IMPORTANCE')
+    
+    
+  })
+  
+  
+  
+  
+  
+  
+}
+
+
+shinyApp(ui=ui, server=server)
